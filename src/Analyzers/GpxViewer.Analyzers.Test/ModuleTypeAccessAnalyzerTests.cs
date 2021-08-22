0efc19479c1d39c601a74e52264206f4c07aea69
Roland König <roland.koenig@rolandk.de>
2021-08-22 09:26:51 +0200

    Added analyzer project for some coding checks

---
+++ src/Analyzers/GpxViewer.Analyzers.Test/ModuleTypeAccessAnalyzerTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Testing.MSTest;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GpxViewer.Analyzers.Test.Verifiers;
+
+namespace GpxViewer.Analyzers.Test
+{
+    [TestClass]
+    public class ModuleTypeAccessAnalyzerTests
+    {
+        [TestMethod]
+        public async Task MainModuleType_BadCase()
+        {
+            var test = @"
+namespace GpxViewer.Modules.Testing
+{
+    internal class {|#0:TestingModule|}
+    {
+        //...
+    }
+}";
+            var expected = CSharpAnalyzerVerifier<ModuleTypeAccessAnalyzer>
+                .Diagnostic(ModuleTypeAccessAnalyzer.DIAGNOSTIC_ID)
+                .WithLocation(0)
+                .WithArguments("TestingModule", "Internal", "Public");
+
+            await CSharpAnalyzerVerifier<ModuleTypeAccessAnalyzer>.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task MainModuleType_GoodCase()
+        {
+            var test = @"
+namespace GpxViewer.Modules.Testing
+{
+    public class {|#0:TestingModule|}
+    {
+        //...
+    }
+}";
+
+            await CSharpAnalyzerVerifier<ModuleTypeAccessAnalyzer>.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task MainModuleType_InterfaceType_BadCase()
+        {
+            var test = @"
+namespace GpxViewer.Modules.Testing.Interface.Messages
+{
+    internal class {|#0:TestMessage|}
+    {
+        //...
+    }
+}";
+            var expected = CSharpAnalyzerVerifier<ModuleTypeAccessAnalyzer>
+                .Diagnostic(ModuleTypeAccessAnalyzer.DIAGNOSTIC_ID)
+                .WithLocation(0)
+                .WithArguments("TestMessage", "Internal", "Public");
+
+            await CSharpAnalyzerVerifier<ModuleTypeAccessAnalyzer>.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task MainModuleType_InterfaceType_GoodCase()
+        {
+            var test = @"
+namespace GpxViewer.Modules.Testing.Interface.Messages
+{
+    public class {|#0:TestMessage|}
+    {
+        //...
+    }
+}";
+
+            await CSharpAnalyzerVerifier<ModuleTypeAccessAnalyzer>.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task MainModuleType_LogicType_BadCase()
+        {
+            var test = @"
+namespace GpxViewer.Modules.Testing.Logic
+{
+    public class {|#0:TestLogicClass|}
+    {
+        //...
+    }
+}";
+            var expected = CSharpAnalyzerVerifier<ModuleTypeAccessAnalyzer>
+                .Diagnostic(ModuleTypeAccessAnalyzer.DIAGNOSTIC_ID)
+                .WithLocation(0)
+                .WithArguments("TestLogicClass", "Public", "Internal");
+
+            await CSharpAnalyzerVerifier<ModuleTypeAccessAnalyzer>.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task MainModuleType_LogicType_GoodCase()
+        {
+            var test = @"
+namespace GpxViewer.Modules.Testing.Logic
+{
+    internal class {|#0:TestLogicClass|}
+    {
+        //...
+    }
+}";
+
+            await CSharpAnalyzerVerifier<ModuleTypeAccessAnalyzer>.VerifyAnalyzerAsync(test);
+        }
+    }
+}
---
+++ src/Analyzers/GpxViewer.Analyzers/ModuleTypeAccessAnalyzer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace GpxViewer.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ModuleTypeAccessAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DIAGNOSTIC_ID = "GpxViewerAnalyzers";
+
+        private const string TITLE = "Module Type accessibility";
+        private const string MESSAGE_FORMAT = "Type '{0}' has invalid accessibility. Current: {1}, expected: {2}";
+        private const string DESCRIPTION = "Only interface types and main type of modules are public, all others musst be internal";
+        private const string CATEGORY = "GpxViewer Modules";
+
+        private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(DIAGNOSTIC_ID, TITLE, MESSAGE_FORMAT, CATEGORY, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: DESCRIPTION);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(s_rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            var fullNamespace = namedTypeSymbol.ContainingNamespace.ToString();
+            if (!fullNamespace.StartsWith("GpxViewer.Modules")) { return; }
+
+            var splittedNamespace = fullNamespace.Split('.');
+            if (splittedNamespace.Length < 3) { return; }
+            var moduleName = splittedNamespace[2];
+
+            var namespaceInterface = $"GpxViewer.Modules.{moduleName}.Interface";
+            var moduleMainTypeNamespace = $"GpxViewer.Modules.{moduleName}";
+            var moduleMainTypeName = $"{moduleName}Module";
+
+            var actTypeNamespace = namedTypeSymbol.ContainingNamespace.ToString();
+            var actTypeName = namedTypeSymbol.Name;
+
+            var expectedAccessibility = Accessibility.Internal;
+            if (actTypeNamespace.StartsWith(namespaceInterface))
+            {
+                expectedAccessibility = Accessibility.Public;
+            }
+            else if (actTypeNamespace.Equals(moduleMainTypeNamespace, StringComparison.Ordinal) &&
+                     actTypeName.Equals(moduleMainTypeName, StringComparison.Ordinal))
+            {
+                expectedAccessibility = Accessibility.Public;
+            }
+
+            if (namedTypeSymbol.DeclaredAccessibility != expectedAccessibility)
+            {
+                var diagnostic = Diagnostic.Create(s_rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name, namedTypeSymbol.DeclaredAccessibility, expectedAccessibility);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
